127304b70198cfabf99953ddef50323f27369b51
Danilo Guimarães <guimaraesdjl@gmail.com>
2018-07-31 09:12:07 -0300

    Script de criação da tabela nfe

---
+++ src/main/resources/create_tables.sql
@@ -0,0 +1,22 @@
+CREATE SEQUENCE public.nfe_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.nfe_id_seq
+  OWNER TO postgres;
+
+CREATE TABLE public.nfe
+(
+  id bigint NOT NULL DEFAULT nextval('nfe_id_seq'::regclass),
+  nome character varying(255),
+  conteudo text, 
+  data_hora_emissao timestamp withou time zone,
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.nfe
+  OWNER TO postgres;
